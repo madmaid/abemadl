1abcb9b6b2fa4c79344f9ff06a2c046a76f642c0
madmaid <madmaid@users.noreply.github.com>
2020-05-07 08:06:25 +0900

    refactor

--- src/index.ts
+++ src/index.ts
@@ -177,6 +177,7 @@ async function crawl(rawUrlsPath: string, rawRecordedDir: string ){
 
 
     let newLog = log;
+    // append a log
     downloadedLog.forEach((program: Recorded) => {
         const alreadyDownloaded: Episode[] = newLog[program.url].episodes || [];
         program.episodes = [...alreadyDownloaded, ...program.episodes]
